04af3c42f203faefa8e1cc5009d515deccdcba36
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2022-08-12 23:32:07 +0200

    Warp2D

--- project/objects/level.gd
+++ project/objects/level.gd
@@ -11,6 +11,9 @@ func set_size(val : Vector2) -> void:
 func get_bounds() -> Rect2:
 	return Rect2(position, size)
 
+func is_point_in_bounds(p : Vector2) -> bool:
+	return get_bounds().has_point(p)
+
 func _draw() -> void:
 	if Engine.editor_hint:
 		draw_rect(Rect2(Vector2.ZERO, size), Color.greenyellow, false, 3.0)
---
+++ project/objects/warp.gd
@@ -0,0 +1,58 @@
+extends Area2D
+tool
+
+class_name Warp2D
+
+export(String, FILE) var level_scene_path
+
+export(bool) var displace_using_level_bounds
+
+var _warping : bool
+
+func _ready() -> void:
+	init()
+	set_process(false)
+	
+	if !Engine.editor_hint:
+		connect("body_entered", self, "_on_body_entered")
+		connect("body_exited", self, "_on_body_exited")
+
+func _process(delta: float) -> void:
+	if !_warping && !player_is_in_level():
+		warp()
+
+func _on_body_entered(body) -> void:
+	if Game.player && body == Game.player:
+		set_process(true)
+
+func _on_body_exited(body) -> void:
+	if Game.player && body == Game.player:
+		set_process(false)
+
+func player_is_in_level() -> bool:
+	return LevelManager.get_current_level().is_point_in_bounds(Game.player.global_position)
+
+func init() -> void:
+	collision_mask = 0
+	collision_layer = 0
+	set_collision_mask_bit(Util.COLLISION_MASK_BIT.PLAYER, true)
+	monitorable = false
+
+func warp() -> void:
+	if displace_using_level_bounds:
+		var _bounds = LevelManager.get_current_level().get_bounds()
+		var _pos = Game.player.global_position
+		if _pos.x < _bounds.position.x:
+			_pos.x += _bounds.size.x
+		elif _pos.x > _bounds.end.x:
+			_pos.x -= _bounds.size.x
+		
+		if _pos.y < _bounds.position.y:
+			_pos.y += _bounds.size.y
+		elif _pos.y > _bounds.end.y:
+			_pos.y -= _bounds.size.y
+		Game.player.global_position = _pos
+	
+	
+	LevelManager.change_from_path(level_scene_path)
+	_warping = true
