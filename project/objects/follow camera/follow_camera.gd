5a9b3c51b93ab15212462236358748eb159bfc41
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2022-08-12 21:01:45 +0200

    Level class
    - You can define a room's bounds in the level class
    - use_room_bounds in camera will attempt to set the limits using data from the level

--- project/objects/follow camera/follow_camera.gd
+++ project/objects/follow camera/follow_camera.gd
@@ -6,10 +6,24 @@ export(String) var target_group_name = "player"
 
 export(bool) var recheck_post_exit
 
+export(bool) var use_level_bounds = true
+
 var target : Node2D
+
+func set_limits(rect : Rect2) -> void:
+	limit_left = rect.position.x
+	limit_right = rect.position.x + rect.size.x
+	limit_top = rect.position.y
+	limit_bottom = rect.position.y + rect.size.y
+
+func set_limits_level(level : Level2D) -> void:
+	if level:
+		set_limits(level.get_bounds())
 
 func _ready() -> void:
 	get_tree().connect("idle_frame", self, "update_target", [], CONNECT_ONESHOT)
+	if use_level_bounds:
+		set_limits_level(LevelManager.get_current_level())
 
 func _process(delta: float) -> void:
 	follow_target()
---
+++ project/objects/level.gd
@@ -0,0 +1,16 @@
+extends Node2D
+tool
+
+class_name Level2D
+
+export(Vector2) var size = Vector2(1024.0, 608.0) setget set_size
+func set_size(val : Vector2) -> void:
+	size = val
+	update()
+
+func get_bounds() -> Rect2:
+	return Rect2(position, size)
+
+func _draw() -> void:
+	if Engine.editor_hint:
+		draw_rect(Rect2(Vector2.ZERO, size), Color.greenyellow, false, 3.0)
