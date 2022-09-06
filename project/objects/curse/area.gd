cca48a0b54a08456e5d544e6b2188c612d8cdd2e
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2022-09-06 16:57:01 +0200

    Added hiding spot and moving curse

--- project/objects/curse/area.gd
+++ project/objects/curse/area.gd
@@ -1,18 +1,38 @@
-extends Sprite
-tool
+extends KinematicBody2D
+
+## A node that manages one curse area
 
 class_name CurseArea2D
+
+export(Vector2) var velocity
+
+var shape : CollisionShape2D
+var particles : Particles2D
 
 const PARTICLES_PER_PIXEL = 0.05
 const PARTICLES_SCENE = preload("res://objects/curse/curse.tscn")
 
+func _init() -> void:
+	set("motion/sync_to_physics", true)
+	set_collision_layer_bit(Util.COLLISION_MASK_BIT.KILLER, true)
+
 func _ready() -> void:
-	texture = preload("res://reusables/white_square.png")
-	modulate = Color.from_hsv(0.35, 0.5, 1.0, 0.5)
+	for n in get_children():
+		if !(n is CollisionShape2D):
+			continue
+		if !(n.shape is RectangleShape2D):
+			continue
+		shape = n
+		particles = create_curse_particles(global_position, (n.shape as RectangleShape2D).extents)
+		CurseUtil.get_curseport(self).add_child(particles)
+		break
 	
-	if !Engine.editor_hint:
-		visible = false
-		CurseUtil.get_curseport(self).add_child(create_curse_particles(global_position, texture.get_size() * scale / 2.0))
+	if !shape || !particles:
+		printerr("CurseArea2D: Cannot generate curse, no child CollisionShape2D with RectangleShape2D found")
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+	particles.global_position = shape.global_position
 
 static func create_curse_particles(pos : Vector2, size : Vector2) -> Particles2D:
 	var _particles = PARTICLES_SCENE.instance()
--- project/objects/player/states/hiding.gd
+++ project/objects/player/states/hiding.gd
@@ -2,6 +2,7 @@ extends PlayerState
 
 func enter() -> void:
 	player.modulate = Color.from_hsv(0.5, 0.5, 1.0)
+	player.set_collision_mask_bit(Util.COLLISION_MASK_BIT.KILLER, false)
 
 func physics_process(delta: float) -> String:
 	if Input.is_action_pressed("up"):
@@ -11,3 +12,4 @@ func physics_process(delta: float) -> String:
 
 func exit() -> void:
 	player.modulate = Color.white;
+	player.set_collision_mask_bit(Util.COLLISION_MASK_BIT.KILLER, true)
