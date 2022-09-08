f6d54ba4ef374f3f58157161c90dc1ae13293245
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2022-09-08 12:42:05 +0200

    Cave of beginnings

--- project/objects/player/particles/boost.gd
+++ project/objects/player/particles/boost.gd
@@ -3,5 +3,5 @@ extends Particles2D
 onready var prev_position : Vector2 = position
 
 func _process(delta : float) -> void:
-	process_material.set_shader_param("offset", (position - prev_position) * -1.0)
+#	process_material.set_shader_param("offset", (position - prev_position) * -1.0)
 	prev_position = position
