48cb6fd1257db03f84e6dede295153239d91c5e1
Jmannorway <50552091+Synthasmagoria@users.noreply.github.com>
2022-09-16 09:33:33 +0200

    Down dash update
    - Down dash is now locked in
    - You create particles when you land using a downdash

--- project/objects/player/states/down_dash.gd
+++ project/objects/player/states/down_dash.gd
@@ -3,6 +3,7 @@ extends PlayerState
 var gravity = 2000.0
 var max_fall_velocity = 2000.0
 var enter_boost = 400.0
+const LAND_PARTICLES = preload("res://objects/player/particles/land.tscn")
 
 func enter() -> void:
 	player.velocity.y += enter_boost
@@ -18,10 +19,10 @@ func physics_process(delta : float) -> String:
 	if player.is_on_floor():
 		return "Grounded"
 	else:
-		if Input.is_action_pressed("down") && Input.is_action_pressed("jump"):
-			return KEEP_STATE
-		else:
-			return POP_STATE
+		return KEEP_STATE
 
 func exit() -> void:
 	player.modulate = Color.white
+	var _feet = player.get_node("Feet").global_position
+	ParticleManager.burst_particles(LAND_PARTICLES, _feet)
+	ParticleManager.burst_particles(LAND_PARTICLES, _feet).scale.x = -1.0
